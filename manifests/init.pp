e7327fcbd7f9204460b50b1092d689837f50050c
cleverlight <github.account@cleveraspaint.com>
2015-03-25 18:19:33 +0000

    introduced tcp_out_additions to allow for different outgoing tcp ports from a common shared set

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,8 @@ class docsf (
   $testing = '0',
   $tcp_in = '80,443,15022',
   # ftp (21) for yum, ssh (22), smtp (25, 2525, 587), web (80, 443), samba (139), squid proxy (3128), mysql client (3306), puppet (8139,8140), puppetdb (8081), non-std ssh (15022), nrpe (15666), elasticsearch (9200,9300:9400)
-  $tcp_out = '21,22,25,80,443,587,139,2525,3128,3306,8139,8140,8081,15022,15666,9200,9300:9400',
+  $tcp_out = '21,22,25,80,443,587,139,2525,3128,3306,8139,8140,8081,15022,15666',
+  $tcp_out_additions = '',
   $udp_in = '',
   $udp_out = '123',
   $syslog_check = '3600',
@@ -51,6 +52,9 @@ class docsf (
   # begin class
 
 ) inherits docsf::params {
+
+  # concatentate enabled outgoing port list
+  $resolved_tcp_out = "${tcp_out},${tcp_out_additions}"
 
   if ! defined(Package['perl']) {
     package { 'perl' : ensure => 'present', }      
