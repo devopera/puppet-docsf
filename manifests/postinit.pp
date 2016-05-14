2f2a11c46783ee13247f136873ff51969e8b1def
cleverlight <github.account@cleveraspaint.com>
2016-05-14 08:48:06 +0200

    added an extra debug message to show current status of TCP_IN line in csf.conf

--- manifests/postinit.pp
+++ manifests/postinit.pp
@@ -9,6 +9,7 @@ class docsf::postinit (
   $file_replace = $docsf::params::partial_tcp_in,
   $file_target = '/etc/csf/csf.conf',
   $min_size = 10,
+  $postamble = '\n\n',
 
   # end of class arguments
   # ----------------------
@@ -23,14 +24,18 @@ class docsf::postinit (
     command => "cp ${file_target} ${tmp_dir}/docsf_postinit_presub && awk 'BEGIN{getline l < \"${file_replace}\"}/${match}/{gsub(\"${match}\",l)}1' ${file_target} > ${tmp_dir}/docsf_postinit_sub_tcp_in && mv ${tmp_dir}/docsf_postinit_sub_tcp_in ${file_target}",
     # don't substitute port list if for any reason we failed to generate it
     onlyif => "test `wc -c '${file_replace}' | cut -f 1 -d ' '` -ge ${min_size}",
+  }->
+  # display the status of the TCP_IN for csf.conf
+  exec { 'docsf_postinit_sub_readback' :
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+    command => "cat /etc/csf/csf.conf | grep '^TCP_IN = ' | awk ' { print \"'>>>>>>>>>>> \"\$0\"${postamble}\"; } '",
+    logoutput => true,
   }
 
-  $preamble = '*********** '
-  $postamble = '\n\n'
   # dump substitution out to console for double-checking
   exec { 'docsf_postinit_sub_display' :
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
-    command => "cat ${file_replace} | awk ' { print \"${preamble}\"\$0\"${postamble}\"; } '",
+    command => "cat ${file_replace} | awk ' { print \"'*********** \"\$0\"${postamble}\"; } '",
     logoutput => true,
   }
 }
