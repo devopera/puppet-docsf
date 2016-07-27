f6206677e11daf6c59086a5f6a93c47da9d4d69e
cleverlight <github.account@cleveraspaint.com>
2016-07-27 18:07:43 +0200

    added support for systemd-based distributions (thanks e-it)

--- lib/facter/configserver_firewall.rb
+++ lib/facter/configserver_firewall.rb
@@ -1,7 +1,7 @@
 Facter.add("configserver_firewall") do
   has_weight 100
   setcode do
-    if File.exist? "/etc/init.d/csf"
+    if (File.exist? "/usr/lib/systemd/system/csf.service") || (File.exist? "/etc/init.d/csf")
       "true"
     else
       "false"
