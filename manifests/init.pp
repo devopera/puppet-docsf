d686362c78a3c62c9dc8e55e3b545b2d27af4a14
cleverlight <cleverlight@users.noreply.github.com>
2019-08-07 20:16:38 +0200

    relaxed the default port flooding settings

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,8 +35,8 @@ class docsf (
   $connlimit = '22;5,80;20,443;20',
   # sequent connection limit: block a certain number of requests within a certain timeframe
   # - block after 10 SSH connections within 1 minute, for 1 minute
-  # - block after 50 HTTP(S) connections in 5 seconds, for 5 seconds
-  $portflood = '22;tcp;10;60,80;tcp;50;5,443;tcp;50;5',
+  # - block after 50 HTTP(S) connections in 10 seconds, for 10 seconds
+  $portflood = '22;tcp;10;60,80;tcp;50;10,443;tcp;50;10',
 
   # increase process tracking time from 1800 to 3600 by default
   $pt_limit = '3600',
